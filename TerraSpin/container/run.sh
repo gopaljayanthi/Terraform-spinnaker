40b63dc445dbb468d22473c2fd454e12d7ca91f8
gopaljayanthi <56107926+gopaljayanthi@users.noreply.github.com>
2019-11-12 19:44:47 +0530

    Update run.sh

--- TerraSpin/container/run.sh
+++ TerraSpin/container/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-nohup java -Dspring.config.location=/home/terraspin/opsmx/app/config/application.properties  -jar /home/terraspin/opsmx/app/TerraSpin.jar > /home/terraspin/opsmx/app/terraspin.log 2>&1 &
-tail -f /home/ubuntu/logs/data_wrapper.log &
-while :; do
-  sleep 100
-done
+#nohup java -Dspring.config.location=/home/terraspin/opsmx/app/config/application.properties  -jar /home/terraspin/opsmx/app/TerraSpin.jar > /home/terraspin/opsmx/app/terraspin.log 2>&1 &
+java -Dspring.config.location=/home/terraspin/opsmx/app/config/application.properties  -jar /home/terraspin/opsmx/app/TerraSpin.jar
+#tail -f /home/ubuntu/logs/data_wrapper.log &
+#while :; do
+#  sleep 100
+#done
 echo "started terraspin service..."
